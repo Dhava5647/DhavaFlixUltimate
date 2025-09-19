9a027a78811a196045657dbbc55e445b70de8c47
Dhava5647 <butlegar814@gmail.com>
2025-09-19 21:18:25 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  // This is the important part that fixes the images
+  images: {
+    domains: ['image.tmdb.org'],
+  },
+
   webpack: (config) => {
     config.watchOptions = {
       poll: 1000,
